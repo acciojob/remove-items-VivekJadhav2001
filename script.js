a8df1f1563a336860ef4df3beab3e8c59694fcf1
Vivek Jadhav <vivek.jadhav200127@gmail.com>
2025-09-20 20:08:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const removeBtn = document.querySelector("input[type='button']")
+            const selectBar = document.getElementById("colorSelect")
+            // console.log(selectBar, "Drop Down")
+            removeBtn.addEventListener("click",removeColorElement)
+
+            function removeColorElement(){
+                const selectIndex = selectBar.selectedIndex
+                // console.log(selectIndex)
+
+                if(selectIndex !== -1){
+                    selectBar.remove(selectIndex)
+                }
+
+            }
